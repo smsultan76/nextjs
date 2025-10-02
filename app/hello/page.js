95e330764f11f63436026cef813a9066a6399b14
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2025-10-02 22:43:36 +0600

    Basic hello world print

---
+++ app/hello/page.js
@@ -0,0 +1,24 @@
+export default function Hello(){
+    return(
+    <div style={{ 
+      padding: '2rem', 
+      textAlign: 'center',
+      backgroundColor: '#f5f5f5',
+      minHeight: '100vh'
+    }}>
+      <h1 style={{ 
+        fontSize: '3rem', 
+        color: '#333',
+        marginBottom: '1rem'
+      }}>
+        Hello World!
+      </h1>
+      <p style={{ 
+        fontSize: '1.2rem', 
+        color: '#666' 
+      }}>
+        Welcome to my new Next.js page!
+      </p>
+    </div>
+  )
+}
